58c41d3fa01e3e43662994967564606232d8d6a0
shu <cp.aigis18@gmail.com>
2023-07-27 17:34:47 +0900

    mod

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import Navbar from './Navbar/Navbar'
 
-const Layout = ({children}) => {
+type LayoutProps = { children: ReactNode; };
+
+const Layout = ({children}: LayoutProps) => {
   return (
     <div>
         <Navbar />
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,74 +1,75 @@
-import { getSingPost } from '@/lib/notionAPI'
-import { getAllPosts } from '@/lib/notionAPI'
-import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
-import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
-import {vscDarklPlus} from 'react-syntax-highlighter/dist/cjs/styles/prism'
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import { getAllPosts, getSingPost } from "../../lib/notionAPI";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import Link from "next/link";
+// import { defaultStyle } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
 export const getStaticPaths = async () => {
-    const allPosts = await getAllPosts()
-    const paths = allPosts.map(({slug}) =>({params: {slug}} ))
-    return {
-        paths,
-        fallback: "blocking", // 404ページを表示する場合はblockingを指定する
-    }
-}
+  const allPosts = await getAllPosts();
+  const paths = allPosts.map(({ slug }) => ({ params: { slug } }));
 
-export const getStaticProps = async ({params}) => {
-    const post = await getSingPost(params.slug)
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
 
-    return {
-        props: {
-            post,
-        },
-        revalidate: 60 * 60 * 6,
-    }
-  }
+export const getStaticProps = async ({ params }) => {
+  const post = await getSingPost(params.slug);
 
-function Post({post}) {
+  return {
+    props: {
+      post,
+    },
+    revalidate: 10,
+  };
+};
+
+const Post = ({ post }) => {
   return (
     <section className="container lg:px-2 px-5 h-screen lg:w-2/5 mx-auto mt-20">
-        <h2 className="w-full text-2xl font-medium">{post.metadata.title}</h2>
-        <div className="border-b-2 w-1/3 mt-1 border-sky-900"></div>
-        <span className="text-gray-500">{post.metadata.date}</span>
-        <br />
-        {post.metadata.tags.map((tag:string, index: number) => (
-             <p className="text-white bg-sky-900 rounded-xl font-medium mt-2 px-2 inline-block mr-2" key={index}>
-              <Link href={`posts/tag/${tag}/page/1`}>
-             {tag}</Link></p>
-
-            ))
-        }
-
-        <div className="mt-10 font-medium">
-            <ReactMarkdown
-            components={{
-              code({node, inline, className, children }) {
-              const match = /language-(\w+)/.exec(className || '')
+      <h2 className="w-full text-2xl font-medium">{post.metadata.title}</h2>
+      <div className="border-b-2 w-1/3 mt-1 border-sky-900"></div>
+      <span className="text-gray-500">Posted date at {post.metadata.date}</span>
+      <br />
+      {post.metadata.tags.map((tag: string, index: number) => (
+        <p
+          className="text-white bg-sky-900 rounded-xl font-medium mt-2 px-2 inline-block mr-2"
+          key={index}
+        >
+          <Link href={`/posts/tag/${tag}/page/1`}>{tag}</Link>
+        </p>
+      ))}
+      <div className="mt-10 font-medium">
+        <ReactMarkdown
+          components={{
+            code({ node, inline, className, children }) {
+              const match = /language-(\w+)/.exec(className || "");
               return !inline && match ? (
                 <SyntaxHighlighter
-                  style={vscDarklPlus}
+                  style={vscDarkPlus}
                   language={match[1]}
                   PreTag="div"
                 >
-                  {String(children).replace(/\n$/, '')}
+                  {String(children).replace(/\n$/, "")}
                 </SyntaxHighlighter>
               ) : (
                 <code>{children}</code>
-              )
-            }
+              );
+            },
           }}
         >
-        {post.markdown}
-             </ReactMarkdown>
+          {post.markdown}
+        </ReactMarkdown>
 
         <Link href="/">
-            <p className="text-sky-900 font-medium mt-10 pb-20">←Back to Home</p>
+          <span className="pb-20 block mt-3 text-sky-900">←ホームに戻る</span>
         </Link>
-        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
